58a445607eb89d1344b00aa86e1faf6bba2b23bc
Administrator <Administrator@6SYKVV4VAB4N3IZ>
2018-12-17 18:15:20 +0800

    elastic search demo init.

---
+++ elasticSearchDemo/src/main/java/com/navy/config/ElasticSearchConfig.java
@@ -0,0 +1,31 @@
+package com.navy.config;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+	
+	@Bean
+    public TransportClient transportClient() throws UnknownHostException{
+
+        TransportAddress node = new TransportAddress(InetAddress.getByName("47.107.189.213"),9300);
+        
+        Settings settings = Settings.builder()
+                .put("cluster.name","elasticSearchCluster")
+                .build();
+
+        PreBuiltTransportClient preBuiltTransportClient = new PreBuiltTransportClient(settings);
+
+        preBuiltTransportClient.addTransportAddress(node);
+
+        return preBuiltTransportClient;
+    }
+}
---
+++ elasticSearchDemo/src/main/java/com/navy/controller/TestController.java
@@ -0,0 +1,40 @@
+package com.navy.controller;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+	
+	@Autowired
+	private TransportClient transportClient;
+	
+    @PostMapping("/test/add")
+    public Object add(@RequestBody Map<String, Object> map){
+        try {
+            XContentBuilder content = XContentFactory.jsonBuilder()
+                    .startObject()
+                    .field("title", map.get("title"))
+                    .field("author", map.get("author"))
+                    .field("wordCount", map.get("wordCount"))
+                    .endObject();
+
+            IndexResponse result = this.transportClient.prepareIndex("title", "novel")
+                    .setSource(content)
+                    .get();
+            return result;
+        }catch (IOException e){
+           return e;
+        }
+    }
+}
